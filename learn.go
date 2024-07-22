690d0256eb2ab7f5e697ddc248058e8d610d2bbe
souravdhar199 <souravdhar1999@gmail.com>
2024-07-22 12:13:44 -0400

    Learned basic

--- learn.go
+++ learn.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-	fmt.Print("Hello world")
+	fmt.Println("Hello world")
+	test1(19, "WWW")
+	fmt.Println(fucnThatreturnint("String 1", "String 2"))
+
+}
+
+func test1(value int, value2 string) {
+	fmt.Println("This is a random int from 0 to 100 -->", rand.Intn(100))
+	fmt.Println(value, value2)
+}
+
+func fucnThatreturnint(v, v1 string) int {
+	fmt.Println("Passing data -->", v, v1)
+	return 12
 }
